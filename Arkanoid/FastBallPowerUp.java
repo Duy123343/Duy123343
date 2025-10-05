aa13091adef92a65fc2ea751f6f35ce3a2781486
Duy123343 <hpduy.tb@gmail.com>
2025-10-05 21:19:35 +0700

    Add files via upload

---
+++ Arkanoid/FastBallPowerUp.java
@@ -0,0 +1,27 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+
+import java.awt.Color;
+
+public class FastBallPowerUp extends PowerUp {
+    private double speedMultiplier;
+
+    public FastBallPowerUp(double startX, double startY) {
+        super(startX, startY, 32, 16, 200);
+        this.color = Color.RED;
+        this.speedMultiplier = 1.5;
+    }
+
+    @Override
+    public void apply(Ball ball, Paddle paddle) {
+        if (!used) {
+            ball.setSpeed(ball.getSpeed() * speedMultiplier);
+            used = true;
+            deactive();
+        }
+    }
+}
